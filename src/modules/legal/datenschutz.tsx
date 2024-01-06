a8a536d5a4761f54ee30eb2aefe674a78c209d3e
Christian Scheub <christian.developer.app@gmail.com>
2024-01-06 18:09:54 +0100

    Add Electron Support Part 1

--- src/modules/legal/datenschutz.tsx
+++ src/modules/legal/datenschutz.tsx
@@ -1,8 +1,12 @@
 import { Card } from "react-bootstrap";
 import { datenschutz_text } from "../app_configuration/app_texts";
 import CodeToTextParser from "./codeToTextParser";
+import { useNavigate } from "react-router-dom";
+import { MdArrowBack } from "react-icons/md";
+
 
 const Datenschutz: React.FC = () => {
+  const navigate = useNavigate();
 
   return (
     <div
@@ -11,6 +15,11 @@ const Datenschutz: React.FC = () => {
     }}>
       <div className="after-login-container">
         <Card className="mb-3" style={{ margin: "2vw",backgroundColor: "#49454F", color: "white" }}>
+        <MdArrowBack
+              style={{ cursor: "pointer" }}
+              onClick={() => navigate(-1)}
+              data-testid="backButton"
+            />
           <Card.Header as="h2">
             Infos
           </Card.Header>
