c12c111fd726f13b33cd62b27958af04e0d5d5ae
Christian Scheub <christian.developer.app@gmail.com>
2025-10-14 02:25:44 +0200

    migrate to vite build

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+
+// Dynamically import the plugin to avoid ESM `require` issues when esbuild runs
+export default async () => {
+  const react = (await import('@vitejs/plugin-react')).default
+  return defineConfig({
+    plugins: [react()],
+  })
+}
